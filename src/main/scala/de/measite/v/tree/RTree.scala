f3a1d136d5756b92fb87e7176bb926f7f966b3d0
Rene Treffer <treffer@localhost.(none)>
2009-09-04 08:40:40 +0200

    rtree class documentation

--- src/main/scala/de/measite/v/tree/RTree.scala
+++ src/main/scala/de/measite/v/tree/RTree.scala
@@ -5,18 +5,33 @@ import java.util.ArrayList
 import de.measite.v.data.KVector
 import de.measite.v.data.RRectangle
 
+/**
+ * The RTree root class. Mainly intaracting on the root node.
+ */
 case class RTree[T](width: Int) extends RTreeParent {
 
+  /**
+   * The root node of this tree.
+   */
   val root = new RTreeNode[T](width)
 
-  root.parent = this
+  {
+    root.parent = this
+  }
 
+  /**
+   * Add a new position by finding the nearest node and adding
+   * the requested element.
+   */
   def +(position: KVector) : RTreeLeaf[T] = {
     val search = new NearestNodeIterator(position, root)
     var result = search.next()
     result + position
   }
 
+  /**
+   * Return a string representation of this object instance.
+   */
   override def toString() : String = {
     "RTree{" + root + "}"
   }
