dd50e5b737e7fedb29a4b0a61f41b41d926a6e80
Rene Treffer <treffer@localhost.(none)>
2009-09-04 08:37:45 +0200

    state documented

--- src/main/scala/de/measite/v/searchtree/State.scala
+++ src/main/scala/de/measite/v/searchtree/State.scala
@@ -1,9 +1,20 @@
 package de.measite.v.searchtree
 
+/**
+ * A simple search tree state. Terminal states are solutions,
+ * ordering is defined by natural ordering and following (child)
+ * states can be obtained via next.
+ */
 trait State {
 
+  /**
+   * Retrieve the following states (usually the child states).
+   */
   def next() : Array[State]
 
+  /**
+   * Return true if this state is a terminal (e.g. a solution).
+   */
   def isTerminal() : boolean
 
 }
