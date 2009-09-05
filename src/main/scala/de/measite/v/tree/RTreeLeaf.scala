8bbe5c7f979dc3d7f841e53cadd45192eb5973cc
Rene Treffer <treffer@localhost.(none)>
2009-09-05 22:21:22 +0200

    Simplification and documentation for RTreeLeaf

--- src/main/scala/de/measite/v/tree/RTreeLeaf.scala
+++ src/main/scala/de/measite/v/tree/RTreeLeaf.scala
@@ -2,24 +2,28 @@ package de.measite.v.tree
 
 import de.measite.v.data.KVector
 
+/**
+ * A RTree data leaf.
+ */
 case class RTreeLeaf[T](position: KVector) extends RTreeElement {
 
+  /**
+   * The data payload.
+   */
   var data: T = _
 
+  /**
+   * Create a new data node with a custom payload.
+   */
   def this(position: KVector, d: T) {
     this(position)
     data = d
     this
   }
 
-  def setData(d : T) {
-    data = d
-  }
-
-  def getData(d : T) {
-    data
-  }
-
+  /**
+   * Implicit cast to the underlaying data type
+   */
   def coerce : T = {
     data
   }
