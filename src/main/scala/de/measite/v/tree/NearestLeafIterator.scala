d9d9a21ad4dddce794e09276b480b9e73ff2033c
Rene Treffer <treffer@localhost.(none)>
2009-09-08 00:39:52 +0200

    reimplement/fix NearestLeafIterator

--- src/main/scala/de/measite/v/tree/NearestLeafIterator.scala
+++ src/main/scala/de/measite/v/tree/NearestLeafIterator.scala
@@ -1,44 +1,48 @@
 package de.measite.v.tree
 
 import java.lang.UnsupportedOperationException
-import java.util.TreeMap
+import java.util.Comparator
+import java.util.TreeSet
 
 import de.measite.v.data.KVector
 
 class NearestLeafIterator[T](
   position: KVector,
   root: RTreeNode[T]
-) extends java.util.Iterator[RTreeLeaf[T]] {
+) extends java.util.Iterator[RTreeLeaf[T]]
+  with    Comparator[Any]
+{
 
-    private val best = new TreeMap[Double, RTreeLeaf[T]]()
-    private val border = new TreeMap[Double, RTreeNode[T]]()
+    private val border = new TreeSet[RTreeElement](this)
     private var _next : RTreeLeaf[T] = _
     private var _cur  : RTreeLeaf[T] = _
     private var _hasNext = true
 
     {
-      border.put(root.rectangle.distance(position), root)
+      border.add(root)
     }
 
     def hasNext() : boolean = {
-      if (!hasNext) {
+      if (!_hasNext) {
         false
       } else {
-        if (_next eq null) { _next = inext() }
-        if (_next eq null) {
-          _hasNext = false
-        }
+        if (_next eq null) { _next = inext()  }
+        if (_next eq null) { _hasNext = false }
         _hasNext
       }
     }
 
     def next() : RTreeLeaf[T] = {
-      if (!hasNext) {
+      if (!_hasNext) {
         throw new IllegalStateException
       }
-      if (_next eq null) { _next = inext() }
-      _cur  = _next
-      _next = null
+      if (_next eq null) {
+        _cur = inext()
+        _hasNext = (_cur ne null)
+      } else {
+        _cur  = _next
+        _next = null
+      }
       _cur
     }
 
@@ -46,42 +50,59 @@ class NearestLeafIterator[T](
       throw new UnsupportedOperationException
     }
 
-    def inext() : RTreeLeaf[T] = {
-      while (border.size > 0
-         && (best.size == 0 || border.firstKey < best.firstKey)
-      ) {
-        val expandKey = border.firstKey
-        val expandNode = border.get(expandKey)
-        border.remove(expandKey)
-        if (expandNode.isLeafLevel) {
-          var i = 0;
-          while (i < expandNode.childs && expandNode.child(i) != null) {
-              val child = expandNode.child(i).asInstanceOf[RTreeLeaf[T]]
-              best.put(
-                child.position - position,
-                child
-              )
-          }
+    private def inext() : RTreeLeaf[T] = {
+      while (border.size > 0) {
+        val expand = border.pollFirst
+        if (expand.isInstanceOf[RTreeLeaf[T]]) {
+          return expand.asInstanceOf[RTreeLeaf[T]]
         } else {
-          var i = 0;
-          while (i < expandNode.childs && expandNode.child(i) != null) {
-            val child = expandNode.child(i).asInstanceOf[RTreeNode[T]]
-            border.put(
-              child.rectangle.distance(position),
-              child
-            )
-            i += 1
-          }
+          val node = expand.asInstanceOf[RTreeNode[T]]
+          node.child.foreach(
+            c => { if (c ne null) {
+              border.add( c )
+            }}
+          )
         }
       }
-      if (best.size == 0) {
-        null
+      null
+    }
+
+  override def compare(x: Any, y: Any) : int = {
+    val bx = x.isInstanceOf[RTreeNode[T]]
+    val dx =
+      if (bx) {
+        x.asInstanceOf[RTreeNode[T]].rectangle.distance(position)
       } else {
-        val key = best.firstKey
-        val node = best.get(key)
-        best.remove(key)
-        node
+        x.asInstanceOf[RTreeLeaf[T]].position - position
+      }
+    val by = y.isInstanceOf[RTreeNode[T]]
+    val dy =
+      if (by) {
+        y.asInstanceOf[RTreeNode[T]].rectangle.distance(position)
+      } else {
+        y.asInstanceOf[RTreeLeaf[T]].position - position
+      }
+    if (dx != dy) {
+      Math.signum(dx - dy).asInstanceOf[int]
+    } else {
+      if (bx) {
+        if (by) {
+          x.asInstanceOf[RTreeNode[T]].rectangle.compareTo(
+            y.asInstanceOf[RTreeNode[T]].rectangle
+          )
+        } else {
+          -1
+        }
+      } else {
+        if (by) {
+           1
+        } else {
+          x.asInstanceOf[RTreeLeaf[T]].position.compareTo(
+            y.asInstanceOf[RTreeLeaf[T]].position
+          )
+        }
       }
     }
+  }
 
 }
