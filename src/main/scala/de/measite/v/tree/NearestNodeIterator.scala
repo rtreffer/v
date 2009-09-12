e824fe7d6e53095a67cd5a9a517cf0c9a23475a5
Rene Treffer <treffer@localhost.(none)>
2009-09-12 22:12:42 +0200

    simplify NearestNodeIterator

--- src/main/scala/de/measite/v/tree/NearestNodeIterator.scala
+++ src/main/scala/de/measite/v/tree/NearestNodeIterator.scala
@@ -1,45 +1,54 @@
 package de.measite.v.tree
 
-import java.util.TreeMap
+import java.lang.Math.signum
+import java.util.TreeSet
 
 import de.measite.v.data.KVector
 
 case class NearestNodeIterator[T](position: KVector, root: RTreeNode[T]) {
 
-  val best = new TreeMap[Double, RTreeNode[T]]()
-  val border = new TreeMap[Double, RTreeNode[T]]()
+  case class Entry(element: RTreeNode[T]) extends Comparable[Entry] {
+    val score : double = element.rectangle.distance(position)
+    val area  : double = element.rectangle.area1p
+
+    override def compareTo(that : Entry) : int = {
+      if (this.score != that.score) {
+        signum(this.score - that.score).asInstanceOf[int]
+      } else {
+        if (this.area != that.area) {
+          signum(that.area - this.area).asInstanceOf[int]
+        } else {
+          this.element.rectangle.compareTo(that.element.rectangle)
+        }
+      }
+    }
+  }
+
+
+  val border = new TreeSet[Entry]()
 
   {
-    border.put(root.rectangle.distance(position), root)
+    border.add( new Entry(root) )
   }
 
   def next() : RTreeNode[T] = {
-    while (border.size > 0
-       && (best.size == 0 || border.firstKey < best.firstKey)
-    ) {
-      val expandKey = border.firstKey
-      val expandNode = border.remove(expandKey)
-      if (expandNode.isLeafLevel) {
-        best.put(expandKey, expandNode)
+    while (border.size > 0) {
+      val element = border.pollFirst.element
+      if (element.isLeafLevel) {
+        return element
       } else {
-        expandNode.foreach(
-          (i, leaf) => { },
-          (i, node) => {
-            border.put(
-              node.rectangle.distance(position),
-              node
+        var i = 0
+        while (i < element.childs) {
+          border.add(
+            new Entry(
+              element.child(i).asInstanceOf[RTreeNode[T]]
             )
-          }
-        )
+          )
+          i += 1
+        }
       }
     }
-    if (best.size == 0) {
-      null
-    } else {
-      val key = best.firstKey
-      val node = best.remove(key)
-      node
-    }
+    null
   }
 
 }
