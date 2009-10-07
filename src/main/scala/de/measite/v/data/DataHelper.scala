777996a43975e581e604952cd28eeb088599b0be
rtreffer <treffer@treffer-desktop.local>
2009-10-07 23:08:50 +0200

    unique area (subtract overlaps recursive)

--- src/main/scala/de/measite/v/data/DataHelper.scala
+++ src/main/scala/de/measite/v/data/DataHelper.scala
@@ -3,32 +3,55 @@ package de.measite.v.data
 import java.lang.Double._
 import java.util.ArrayList
 
-trait DataHeltper {
+object DataHeltper {
 
   def uniqueArea(rect : Array[RRectangle]) : Double = {
-    if (rect.length == 0) { return 0d }
-    val list = new ArrayList[RRectangle](rect.length)
+    var list = new ArrayList[RRectangle](rect.length + 1)
+    var i = 0
+    while (i < rect.length) {
+      if (rect(i) ne null) {
+        list.add(rect(i))
+      }
+      i += 1
+    }
+    _uniqueArea(list)
+  }
+
+  private def _uniqueArea(rect : ArrayList[RRectangle]) : Double = {
+    if (rect.size == 0) { return 0d }
+    val list = new ArrayList[RRectangle](rect.size)
     var i = 0
     var area = 0d
-    while (i < rect.length) {
+    while (i < rect.size) {
       var j = i + 1
-      val r = rect(i)
-      while (j < rect.length) {
-        val inter = r.intersection(rect(j))
-        val area  = inter.area
-        if (area > 0d) {
-          list.add(inter)
+      val r = rect.get(i)
+      var contained = false
+      while (j < rect.size && !contained) {
+        val inter = r.intersection(rect.get(j))
+        if (inter eq r) {
+          // we are contained in rect[j]
+          contained = true
+        } else
+        if (inter eq rect.get(j)) {
+          contained = true
+          rect.set(j,r)
+        } else {
+          val area  = inter.area
+          if (area > 0d) {
+            list.add(inter)
+          }
+          j += 1
         }
-        j += 1
       }
-      area += r.area
+      if (!contained) {
+        area += r.area
+      }
       i += 1
     }
     if (list.size == 0) {
       area
     } else {
-      val array = list.toArray(new Array[RRectangle](list.size))
-      area - uniqueArea(array)
+      area - _uniqueArea(list)
     }
   }
 
